89a8fcf8aff7cae233a835e6a94e0b01e450a36b
RayannBen <rayann.bentounes@sicara.com>
2026-07-16 16:21:50 +0200

    feat(emojis): add emojis on 3 lines

--- src/components/reactionsPopover.tsx
+++ src/components/reactionsPopover.tsx
@@ -3,7 +3,11 @@ import Txt, { TxtSize } from "~/components/ui/txt";
 import { useGame, useSelfPlayer } from "~/hooks/game";
 
 const ClearReaction = "⊘";
-const Reactions = ["👍", "👎", "👏", "😅", "🤩", "🤭", "🤔", "😬", "♻️", "❤️", "😻"];
+const Reactions = [
+  ["👍", "👏", "🤩", "❤️", "😻", "🎉", "🥳", "🤭", "😂"],
+  ["😅", "🫣", "😬", "🤔", "😱", "🤯", "🤦", "🫠", "😭"],
+  ["👎", "🙃", "♻️", "💅", "💃", "🔥", "🙏", "👀", "❓"],
+];
 
 interface Props {
   onReaction: (reaction: string) => void;
@@ -18,27 +22,33 @@ export default function ReactionsPopover(props: Props) {
   const selfPlayer = useSelfPlayer(game);
 
   return (
-    <div className="flex items-center justify-center ba bw1 bg-white pa2 pt3 pr3 br2 gray" style={props.style}>
-      {Reactions.map((reaction, i) => (
-        <a
-          key={i}
-          className="mh1 pointer"
-          onClick={(e) => {
-            e.stopPropagation();
-            onClose();
-            onReaction(null);
-            setTimeout(() => {
-              onReaction(reaction);
-            });
-          }}
-        >
-          <Txt size={TxtSize.MEDIUM} value={reaction} />
-        </a>
+    <div
+      className="flex flex-column items-center justify-center ba bw1 bg-white pa2 pt3 pr3 br2 gray"
+      style={props.style}
+    >
+      {Reactions.map((row, rowIndex) => (
+        <div key={rowIndex} className="flex items-center justify-center">
+          {row.map((reaction, i) => (
+            <a
+              key={i}
+              className="mh1 pointer"
+              onClick={(e) => {
+                e.stopPropagation();
+                onClose();
+                onReaction(null);
+                setTimeout(() => {
+                  onReaction(reaction);
+                });
+              }}
+            >
+              <Txt size={TxtSize.MEDIUM} value={reaction} />
+            </a>
+          ))}
+        </div>
       ))}
       {selfPlayer.reaction && (
         <a
-          className="ml4 pointer"
-          style={{ marginTop: -6 }}
+          className="mt2 pointer"
           onClick={(e) => {
             e.stopPropagation();
             onClose();
